68c7f3f52273ac2d858776b19aa86c4ae2edc216
Randall Leeds <randall@bleeds.info>
2016-02-21 16:58:51 -0800

    Always enable coverage

--- karma.conf.js
+++ karma.conf.js
@@ -4,7 +4,7 @@ var istanbul = require('browserify-babel-istanbul')
 module.exports = function(config) {
   config.set({
     browsers: ['PhantomJS'],
-    browserify: {debug: true, transform: [babelify]},
+    browserify: {debug: true, transform: [istanbul, babelify]},
     frameworks: ['browserify', 'chai', 'fixture', 'mocha'],
     files: [
       'test/*.js',
@@ -14,11 +14,6 @@ module.exports = function(config) {
       'test/*.js': ['browserify'],
       'test/fixtures/*.html': ['html2js']
     },
-    reporters: ['progress'],
-  })
-
-  if (process.env.npm_config_coverage) config.set({
-    browserify: {debug: true, transform: [istanbul, babelify]},
-    reporters: ['progress', 'coverage', 'coveralls']
+    reporters: ['progress', 'coverage', 'coveralls'],
   })
 }
