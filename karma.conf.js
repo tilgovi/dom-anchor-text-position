171185d8e982a1361a6e553d3297ded38bf62d24
Randall Leeds <randall@bleeds.info>
2016-02-21 17:33:45 -0800

    Cleaner coverage on and off travis

--- karma.conf.js
+++ karma.conf.js
@@ -4,7 +4,7 @@ var istanbul = require('browserify-babel-istanbul')
 module.exports = function(config) {
   config.set({
     browsers: ['PhantomJS'],
-    browserify: {debug: true, transform: [istanbul, babelify]},
+    browserify: {debug: true, transform: [babelify]},
     frameworks: ['browserify', 'chai', 'fixture', 'mocha'],
     files: [
       'test/*.js',
@@ -14,7 +14,16 @@ module.exports = function(config) {
       'test/*.js': ['browserify'],
       'test/fixtures/*.html': ['html2js']
     },
-    reporters: ['progress', 'coverage', 'coveralls'],
+    reporters: ['progress']
+  })
+
+  if (process.env.npm_config_coverage) config.set({
+    browserify: {debug: true, transform: [istanbul, babelify]},
+    reporters: ['progress', 'coverage'],
     coverageReporter: {type: 'lcov'}
+  })
+
+  if (process.env.TRAVIS) config.set({
+    reporters: ['progress', 'coverage', 'coveralls']
   })
 }
